1f6c4e0936e4e691744927bf6e0637260bcee116
Mike-Max <scartman1992@gmail.com>
2021-06-08 00:41:18 +0100

    done hw-10 parcel

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import menu from './menu.json'
+import cards from './templates/cards.hbs';
+import './sass/main.scss';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body')
+const menuRef = document.querySelector('.js-menu');
+const themeBtnRef = document.querySelector('#theme-switch-toggle')
+
+
+
+const createCardsMarkUp = menu.map(cards).join('')
+
+menuRef.insertAdjacentHTML('afterBegin', createCardsMarkUp)
+
+
+themeBtnRef.addEventListener('change',function (e) {
+    if (e.target.checked) {
+        bodyRef.classList.add(Theme.DARK)
+        bodyRef.classList.remove(Theme.LIGHT)
+        localStorage.setItem('Theme',Theme.DARK)
+    }
+    else {
+        bodyRef.classList.add(Theme.LIGHT)
+        bodyRef.classList.remove(Theme.DARK)
+        localStorage.setItem('Theme',Theme.LIGHT)
+    }
+})
+
+const currentTheme = localStorage.getItem('Theme', Theme.LIGHT);
+if (currentTheme === Theme.DARK) {
+    themeBtnRef.cheked = 'true'
+    bodyRef.classList.add(Theme.DARK)
+}
+else {
+    themeBtnRef.cheked = 'false'
+    bodyRef.classList.add(Theme.LIGHT)
+}
+
+
+
